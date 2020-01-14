642d047aaf548f8b9c4af0e5428f9ca9dfdb0410
Reese2596 <reese2596@student.cptc.edu>
2020-01-14 11:44:26 -0800

    Set up ModelBinding Example using HTML Forms

---
+++ ModelBindingSample/Controllers/ModelBindingController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ModelBindingSample.Models;
+
+namespace ModelBindingSample.Controllers
+{
+    public class ModelBindingController : Controller
+    {
+        [HttpGet] //default, not required
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost] //send form data to the server
+        public IActionResult Index(Student s)//Dont have to map to class already done for us
+        {
+            //Ensures our model is in a valid state
+            if (ModelState.IsValid) //All requirements check out
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else //Validation problems 
+            {
+                return View(s);//for the new .Net core you can pass model back to view(s)
+                               //but this is now donr automatically by just returning view
+            }
+        }
+    }
+}
--- ModelBindingSample/Models/Student.cs
+++ ModelBindingSample/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,19 @@ namespace ModelBindingSample.Models
     public class Student
     {
         public int StudentID { get; set; }
+
+        [Display(Name ="Full Name:")] //Data annotations
+        [Required(ErrorMessage ="Full Name is Required")]
         public string FullName { get; set; }
+
+        [Display(Name ="Date of Birth:")]
+        [DataType(DataType.Date)]
         public DateTime DateOfBirth { get; set; }
+
+        [Display(Name ="Favorite Number:")]
+        [Range(1, 99, ErrorMessage ="Pick a Number between 0 and 99")]
+        [Required] //int is always required becasue it is a value type
+                   //unless you make the int nullable(ex:int?)
         public int FavoriteNumber { get; set; }
     }
 }
